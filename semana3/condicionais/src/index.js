10f919adc63ca7a2625e6b03f0f5305c5ae7e245
Henrique <hbattilio@gmail.com>
2020-07-29 16:49:56 -0300

    Condicionais

---
+++ semana3/condicionais/src/index.js
@@ -0,0 +1,104 @@
+//EXERCICIO 1
+// Determina se o valor e par ou impar
+// Passou no teste = PAR
+// Nao passou no teste = IMPAR
+
+// EXERCICIO 2
+// A: Para informar o preco da fruta informada
+// B: 2.25 valor da Maca
+// C: O preco da fruta pera e, R$: 5 ( como nao tem o break para sair do bloco de codigo, ele vai imprimir o valor default)
+
+// EXERCICIO 3
+// A: Pegando a informacao digitada pelo usuario, convertendo para numero, e guardando dentro de uma variavel
+// B: como 10 e maior que 0, entao vai aparecer as duas mensagens, "Esse número passou no teste" e "Essa mensagem é secreta!!!"
+// C: sim, se for digitado um valor que nao execute o bloco,  a variavel 'mensagem' nao vai ter valor atribuido
+
+// EXERCICIO 4
+
+// const idade = Number(prompt("Digite a sua idade"))
+// if(idade>=18){
+//     console.log("Voce tem idade para dirigir")
+// } 
+// else{
+//     console.log("Voce nao tem idade para dirigir")
+// }
+
+// EXERCICIO 5
+
+// const turno = prompt("Qual turno voce estuda").toUpperCase()
+// const turno1 = ["M", "manha"]
+// const turno2 = ["T", "tarde"]
+// const turno3 = ["N", "noite"]
+// if(turno === turno1[0] ){
+// console.log(turno1[1])
+// }
+//     else if(turno === turno2[0]){
+//     console.log(turno2[1])
+//     }
+//         else if(turno === turno3[0]){
+//         console.log(turno3[1])
+//         }
+//         else{
+//             console.log("Turno inexistente")
+//         }
+
+// EXERCICIO 6
+
+// const turno = prompt("Qual turno voce estuda").toUpperCase()
+// const turno1 = ["M", "manha"]
+// const turno2 = ["T", "tarde"]
+// const turno3 = ["N", "noite"]
+// switch(turno){
+//     case turno1[0]:
+//         console.log(turno1[1])
+//         break
+//     case turno2[0]:
+//         console.log(turno2[1])
+//         break
+//     case turno3[0]:
+//         console.log(turno3[1])
+//         break
+//     default:
+//         console.log("Turno inexistente")
+//         break
+// }
+
+// EXERCICIO 7
+
+// const genero = prompt("qual o gênero de filme que vão assistir ?") 
+// const valor = Number(prompt("qual o valor do ingresso ?"))
+// const fantasia = "fantasia"
+
+// if (genero === fantasia && valor <= 15) {
+//     console.log("Bom filme ")
+// }else{
+//     console.log("Escolha outro filme :( ")
+// }
+
+//DESAFIO 1
+
+// const genero = prompt("qual o gênero de filme que vão assistir ?") 
+// const valor = Number(prompt("qual o valor do ingresso ?"))
+// const snack = prompt("qual snack voce vai comprar")
+// const fantasia = "fantasia"
+
+// if (genero === fantasia && valor <= 15) {
+//     console.log("Bom filme!! e com", snack)
+// }else{
+//     console.log("Escolha outro filme :( ")
+// }
+
+//DESAFIO 2
+
+const nome = prompt("Informe seu nome")
+const tipoJogo = prompt("Informe o jogo")
+const etapJogo = prompt("Informe a etapa do jogo")
+const categoria = prompt("Informe a categoria")
+const qtdIngresso = prompt("Informe a quantidade de ingressos")
+
+cons
+
+
+
+
+
---
+++ semana3/operadores/src/index.js
@@ -0,0 +1,40 @@
+// EXERCICIO 1
+
+// Resposta a: false
+//          b: false
+//          c: true
+//          e: boolean
+
+
+// EXERCICIO 2
+// Resposta a: nao tem valor declarado para a variavel 'array'
+//          b: null 
+//          c: 11
+//          d: 3
+//          e: 3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13
+//          f: 8
+
+
+//1:
+
+// const idade = Number(prompt("Qual a sua idade"))
+// const idadeAmigo = Number(prompt("Qual a idade do seu amigo"))
+
+// const boolean = idade >= idadeAmigo
+// console.log("Sua idade é maior do que a do seu melhor amigo?", boolean)
+// const dif = idade - idadeAmigo
+// console.log(dif)
+
+//2: 
+// const numero = Number(prompt("Digite um numero"))
+// let resul = numero%2
+// console.log(resul)
+
+// sempre que for numero par o resul e 0 sempre que e impar e 1
+
+
+//3:
+let tarefa.push ("olaugiusgudi") 
+
+
+
