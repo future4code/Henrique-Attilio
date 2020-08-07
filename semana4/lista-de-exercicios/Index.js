ab1730d33d785bad29a2c5eaf70cee95b0c5be2e
Henrique <hbattilio@gmail.com>
2020-08-07 12:17:46 -0300

    lista de exercicios_1

---
+++ semana4/lista-de-exercicios/Index.js
@@ -0,0 +1,29 @@
+//Exercícios de interpretação de código
+
+//EXERCICIO 1
+//R: Vai fazer a multiplicacao do 'valor em dolar'  * 'cotacao' e  sair o valor convertido 
+//Saida do console: como o valor da contacao nao esta definido, e o valor na chamada na funcao e '100' entao vai ser undefined
+//Ou se o usuario escrever o valor, vai ser 100 * o valor da cotacao
+
+//EXERCICIO 2
+//R: A funcao vai verificar qual e o 'tipo de investimento' e o 'valor' , com isso vai retornar o valor do investimento calculado
+//novoMontante = 165
+//segundoMontante =  vai dar um alert 'TIPO DE INVESTIMENTO INCORRETO' , pois 'Tesouro direto' nao esta declarado como opcao na funcao
+
+//EXERCICIO 3
+//R: vai correr todo o 'array numeros' e verificar se o numero e par vai gravar ele no 'array1' se for impar vai gravar no 'array2'
+// e ao fim vai imprimir o tamanho de 'numeros' , 'array1' e 'array2' (quantidade de numeros)
+// numeros.lenght = 14
+// array1.lenght = 6
+// array2.lenght = 8
+
+//EXERCICIO 4
+//R: Vai passar pelo array 'numeros' inteiro e fazer 2 comparacoes 
+// na primeira comparacao e comparado 'numero < numero1'  sendo que 'numero' igual a o numero do array(nao a posicao)
+//  e 'numero1' e uma string que esta preenchida com 'Infinity', entao toda vez que passar, 
+// vai ser necessariamente falso, nao executando o bloco
+// com isso, passando pelo segundo comparativo 'numero2 > numero' , se 'numero2' for maior que 'numero' entao sera 
+// preenchido 'numero' dentro de 'numero2'
+// que no final 'numero2' vai ser o maior numero do array e e 'numero1' vai estar com 'Infinity'
+
+//Exercícios de Lógica de Programação
